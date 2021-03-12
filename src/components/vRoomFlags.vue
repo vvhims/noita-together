1c67c94e2bff246f57d0f72144ee6b6589ea63be
vvhims <80472274+vvhims@users.noreply.github.com>
2021-03-11 20:45:45 -0500

    Revert "random seed button"
    This reverts commit 27cb2ae3dd58f9d9891281e880f4246f1cb23f0b.

--- src/components/vRoomFlags.vue
+++ src/components/vRoomFlags.vue
@@ -37,9 +37,8 @@
 
             <h2>World seed <vTooltip><span>{{ payload.world.sync_world_seed.tooltip }}</span></vTooltip>
             </h2>
-            <div class="world-seed">
-                <vInput v-model="payload.world.sync_world_seed.value" ref="seedInput"></vInput>
-                <vButton @click="randomizeSeed">Random</vButton>
+            <div>
+                <vInput v-model="payload.world.sync_world_seed.value"></vInput>
             </div>
         </template>
         <div slot="footer" class="centered">
@@ -142,12 +141,6 @@ export default {//braincells where'd ya go
             //console.log({ flags: payload })
             this.$emit("applyFlags", { flags: payload });
         },
-        randomizeSeed() {
-            const seed = Math.floor(Math.random() * 4294967295) + 1
-            // not an amazing way to do it
-            this.$refs.seedInput.$refs.input.value = seed
-            this.$refs.seedInput.$refs.input.dispatchEvent(new Event('input'))
-        },
         close() {
             this.$emit("close");
         },
@@ -170,16 +163,5 @@ export default {//braincells where'd ya go
 .switches > div {
     padding: 0.2em;
     min-width: 180px;
-}
-
-.world-seed {
-    display: flex;
-    width: 100%;
-}
-
-.world-seed .labeled-input{
-    margin-top: auto;
-    padding-bottom: 0;
-    margin-right: 0;
 }
 </style>
